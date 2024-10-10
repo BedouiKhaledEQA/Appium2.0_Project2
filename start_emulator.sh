0d0553b083bec59f206a4efd9ada9fc5f2d2d034
KhaledBEDOUI <bedouikhaled090@gmail.com>
2024-10-10 13:30:57 +0100

    New Commit

--- start_emulator.sh
+++ start_emulator.sh
@@ -8,7 +8,10 @@ export ANDROID_SDK_ROOT="C:/Users/KhaledBEDOUI/AppData/Local/Android/Sdk"
 export PATH="$ANDROID_SDK_ROOT/emulator:$ANDROID_SDK_ROOT/platform-tools:$PATH"
 
 # Démarrer l'émulateur (modifiez le nom selon celui de votre AVD)
-emulator -avd "Pixel_6" -no-snapshot-load -wipe-data &
+if ! emulator -avd "Pixel_6" -no-snapshot-load -wipe-data &; then
+    echo "Échec du démarrage de l'émulateur."
+    exit 1
+fi
 
 # Attendre que l'émulateur soit complètement démarré
 echo "Démarrage de l'émulateur..."
@@ -18,8 +21,8 @@ echo "L'émulateur est prêt."
 
 # Démarrer Appium et afficher les logs dans la console
 echo "Démarrage d'Appium..."
-appium --log-level info  # Modifiez le niveau de log si nécessaire
+nohup appium --log-level info & # Utiliser nohup si nécessaire
 echo "Appium est prêt."
 
-# Attendre quelques secondes pour s'assurer qu'Appium est démarré
+# Attendre que l'émulateur et Appium soient prêts
 sleep 10
