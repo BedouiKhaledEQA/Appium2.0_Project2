a8d1a7286b9c44a215882dc29008711b98bfeb56
KhaledBEDOUI <bedouikhaled090@gmail.com>
2024-10-10 09:26:33 +0100

    New Commit

--- start_emulator.sh
+++ start_emulator.sh
@@ -1,36 +1,22 @@
 #!/bin/bash
 
-# Export Android SDK paths
-export ANDROID_HOME=$HOME/Android/Sdk
+# Exporter les chemins SDK Android
+export ANDROID_HOME=C:/Users/KhaledBEDOUI/AppData/Local/Android/Sdk
 export PATH=$ANDROID_HOME/emulator:$ANDROID_HOME/tools:$ANDROID_HOME/tools/bin:$ANDROID_HOME/platform-tools:$PATH
 
 # Démarrer l'émulateur (modifiez le nom selon celui de votre AVD)
-echo "Démarrage de l'émulateur..."
 emulator -avd "Pixel_6" -no-snapshot-load -no-boot-anim -wipe-data &
 
 # Attendre que l'émulateur soit complètement démarré
-echo "Attendre que l'émulateur se connecte..."
+echo "Démarrage de l'émulateur..."
 adb wait-for-device
-
-# Vérifier si l'émulateur est prêt
-if adb shell getprop sys.boot_completed | grep -m 1 '1'; then
-    echo "L'émulateur est prêt."
-else
-    echo "L'émulateur n'a pas démarré correctement."
-    exit 1
-fi
+adb shell input keyevent 82
+echo "L'émulateur est prêt."
 
 # Démarrer Appium
-echo "Démarrage d'Appium..."
 appium &
 
-# Attendre que Appium soit opérationnel
+# Attendre quelques secondes pour s'assurer qu'Appium est démarré
 sleep 10
 
-# Vérifier si Appium est en cours d'exécution
-if ps aux | grep -q "[a]ppium"; then
-    echo "Appium est prêt."
-else
-    echo "Appium n'a pas démarré correctement."
-    exit 1
-fi
+echo "Appium est prêt."
