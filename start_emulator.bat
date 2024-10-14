8b5d9d1c8847b619267c91bacf0d220642b6e4c4
KhaledBEDOUI <bedouikhaled090@gmail.com>
2024-10-14 11:50:45 +0100

    New Commit

--- start_emulator.bat
+++ start_emulator.bat
@@ -4,24 +4,23 @@ rem Exporter le chemin d'Appium
 set PATH=%PATH%;C:\Users\KhaledBEDOUI\AppData\Roaming\npm
 
 rem Exporter les chemins du SDK Android
-set ANDROID_SDK_ROOT=C:\Users\KhaledBEDOUI\AppData\Local\Android\Sdk
-set PATH=%ANDROID_SDK_ROOT%\emulator;%ANDROID_SDK_ROOT%\platform-tools;%PATH%
+set ANDROID_HOME=C:\Users\KhaledBEDOUI\AppData\Local\Android\Sdk
+set PATH=%ANDROID_HOME%\emulator;%ANDROID_HOME%\platform-tools;%PATH%
 
 rem Démarrer l'émulateur (modifiez le nom selon celui de votre AVD)
 echo Démarrage de l'émulateur...
-start "" "%ANDROID_SDK_ROOT%\emulator\emulator" -avd "Pixel_6" -no-snapshot-load -wipe-data
+start "" "%ANDROID_HOME%\emulator\emulator" -avd "Pixel_6" -no-snapshot-load -wipe-data
 
 rem Attendre que l'émulateur soit complètement démarré
 echo Attente de l'émulateur...
 adb wait-for-device
 adb shell input keyevent 82
-echo L'émulateur est prêt.
+echo "L'émulateur est prêt."
 
 rem Démarrer Appium et afficher les logs dans la console
-echo Démarrage d'Appium...
-cmd /c appium --log-level info
-
-echo Appium est prêt.
+echo "Démarrage d'Appium..."
+start cmd /k appium --log-level info
+echo "Appium est prêt."
 
 rem Attendre quelques secondes pour s'assurer qu'Appium est démarré
 timeout /t 10
